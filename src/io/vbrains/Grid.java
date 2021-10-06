4421f4e5b62e585ed63daa2792e1fd14abba8a46
Varun <varunrao88@hotmail.com>
2021-10-06 19:13:17 +0530

    adding directions

--- src/io/vbrains/Grid.java
+++ src/io/vbrains/Grid.java
@@ -1,31 +1,147 @@
 package io.vbrains;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Grid {
     private int gridSize;
     char[][] contents;
+    private List<Coordinate> coordinates = new ArrayList<>();
+    private enum Direction{
+        HORIZONTAL,
+        VERTICAL,
+        DIAGONAL,
+        HORIZONTAL_INVERSE,
+        VERTICAL_INVERSE,
+        DIAGONAL_INVERSE
+    }
+
+    private class Coordinate{
+        int x;
+        int y;
+        Coordinate(int x,int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public Grid(int gridSize){
         this.gridSize = gridSize;
         this.contents = new char[gridSize][gridSize];
 
-        for(int i=0;i<gridSize;i++)
-            for(int j=0; j<gridSize;j++)
+        for(int i=0;i<gridSize;i++) {
+            for (int j = 0; j < gridSize; j++) {
                 contents[i][j] = '_';
+                coordinates.add(new Coordinate(i,j));
+            }
+        }
     }
 
     public void fillGrid(List<String> words){
+        Collections.shuffle(coordinates);
         for(String word: words){
             if(word.length() >= gridSize)
                 continue;
-            int x = ThreadLocalRandom.current().nextInt(0,gridSize);
-            int y = ThreadLocalRandom.current().nextInt(0,gridSize);
-            for(char c: word.toCharArray()){
-                contents[x][y++] = c;
+            for(Coordinate coordinate:coordinates){
+                int x = coordinate.x;
+                int y = coordinate.y;
+                Direction selectedDirection = getDirectionForFit(word,coordinate);
+                if( selectedDirection != null ){
+                    switch (selectedDirection){
+                        case VERTICAL:
+                            for(char c: word.toCharArray()){
+                                contents[x++][y] = c;
+                            }
+                            break;
+                        case DIAGONAL:
+                            for(char c: word.toCharArray()){
+                                contents[x++][y++] = c;
+                            }
+                            break;
+                        case HORIZONTAL:
+                            for(char c: word.toCharArray()){
+                                contents[x][y++] = c;
+                            }
+                            break;
+                        case HORIZONTAL_INVERSE:
+                            for(char c: word.toCharArray()){
+                                contents[x][y--] = c;
+                            }
+                            break;
+                        case VERTICAL_INVERSE:
+                            for(char c: word.toCharArray()){
+                                contents[x--][y] = c;
+                            }
+                            break;
+                        case DIAGONAL_INVERSE:
+                            for(char c: word.toCharArray()){
+                                contents[x--][y--] = c;
+                            }
+                            break;
+                    }
+                    break;
+                }
             }
         }
+        randomFillGrid();
+    }
+
+    private Direction getDirectionForFit(String word, Coordinate coordinate){
+        List<Direction> directions = Arrays.asList(Direction.values());
+        Collections.shuffle(directions);
+
+        for(Direction direction:directions){
+            if(doesFit(word,coordinate,direction)){
+                return  direction;
+            }
+        }
+        return null;
+    }
+
+    private boolean doesFit(String word, Coordinate coordinate, Direction direction){
+        int wordLength = word.length();
+        switch (direction){
+            case HORIZONTAL:
+                if(coordinate.y + wordLength > gridSize) return false;
+                    for(int i = 0; i< wordLength; i++){
+                        if(contents[coordinate.x][coordinate.y+i]!='_') return false;
+                    }
+                    break;
+            case DIAGONAL:
+                if(coordinate.y + wordLength > gridSize || coordinate.x + wordLength > gridSize) return false;
+                for(int i = 0; i< wordLength; i++){
+                    if(contents[coordinate.x+i][coordinate.y+i]!='_') return false;
+                }
+                break;
+            case VERTICAL:
+                if(coordinate.x + wordLength > gridSize) return false;
+                    for(int i = 0; i< wordLength; i++){
+                        if(contents[coordinate.x+i][coordinate.y]!='_') return false;
+                    }
+                break;
+            case DIAGONAL_INVERSE:
+                if(coordinate.y < wordLength || coordinate.x < wordLength ) return false;
+                for(int i = 0; i< wordLength; i++){
+                    if(contents[coordinate.x-i][coordinate.y-i]!='_') return false;
+                }
+                break;
+            case VERTICAL_INVERSE:
+                if(coordinate.x < wordLength) return false;
+                for(int i = 0; i< wordLength; i++){
+                    if(contents[coordinate.x-i][coordinate.y]!='_') return false;
+                }
+            break;
+            case HORIZONTAL_INVERSE:
+                if(coordinate.y < wordLength) return false;
+                for(int i = 0; i< wordLength; i++){
+                    if(contents[coordinate.x][coordinate.y-i]!='_') return false;
+                }
+                break;
+        }
+        return true;
     }
 
     public void displayGrid(){
@@ -34,6 +150,19 @@ public class Grid {
                 System.out.print(contents[i][j] + " ");
             }
             System.out.println("");
+        }
+    }
+
+    private void randomFillGrid(){
+        String allCapLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        for(int i=0;i<gridSize;i++){
+            for(int j=0; j<gridSize;j++){
+                if(contents[i][j]=='_'){
+                    int randomIndex = ThreadLocalRandom.current().nextInt(0, allCapLetters.length());
+                    contents[i][j] = allCapLetters.charAt(randomIndex);
+                }
+            }
         }
     }
 
--- src/io/vbrains/WordSearchApp.java
+++ src/io/vbrains/WordSearchApp.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class WordSearchApp {
     public static void main(String[] args){
-        List<String> words = Arrays.asList("ONE","TWO","THREE");
-        Grid grid = new Grid(10);
+        List<String> words = Arrays.asList("ONE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT");
+        Grid grid = new Grid(20);
         grid.fillGrid(words);
         grid.displayGrid();
     }
